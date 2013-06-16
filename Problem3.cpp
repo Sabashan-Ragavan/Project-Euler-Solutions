854b9ca0459b320de7e8a5658c0cd7aa3b4d0d58
Sabashan Ragavan <sabes.ragavan@gmail.com>
2013-06-16 16:09:18 -0400

    Problem 3

---
+++ Problem3.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+
+unsigned long long MAGICNUMBER = 600851475143ull; 
+
+bool isPrime(long i); 
+
+using namespace std; 
+
+int main()
+{
+	cout << "The final number printed is the largest prime factor " << endl; 
+	long largestPrime = 1; 
+	
+	for(long i = 2; i <= MAGICNUMBER/2 ; i++)
+	{
+		if(isPrime(i) && (MAGICNUMBER%i == 0))
+		{
+			largestPrime = i; 
+			cout << largestPrime << endl; 
+		}		
+	}
+	
+	return 0; 
+}
+
+bool isPrime(long num)
+{
+	if(num <= 1) 
+		return false; 
+	for(int i = 2; i*i <= num; i++)
+	{
+		if(num%i == 0)
+			return false; 
+	}
+	return true; 
+}
