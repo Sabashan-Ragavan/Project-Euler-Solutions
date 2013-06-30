7f7ea8b6a7ca1fc918b39dbd12d75d56804af93a
Sabashan Ragavan <sabes.ragavan@gmail.com>
2013-06-30 11:03:43 -0400

    Problem 12

---
+++ Problem12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+int numberOfFactors(int num); 
+
+using namespace std;
+
+int main() 
+{	
+	for(int i=1, n =1; ; i = (n*(n+1))/2)
+	{
+		if(numberOfFactors(i) > 500)
+		{
+			cout << i << endl; 
+			break; 
+		}
+		n++;  
+	}	
+	return 0; 
+}
+
+int numberOfFactors(int num)
+{
+	if(num == 1)
+		return 1; 
+	
+	int counter=0;
+	int sqrtVar = (int)sqrt(num); 
+	for(int i=1; i<= sqrtVar; i++)
+		if(num%i == 0)
+			counter+=2; 
+	if(sqrtVar*sqrtVar == num)
+		counter--; 	
+		
+	return counter; 
+}
+
+
+
